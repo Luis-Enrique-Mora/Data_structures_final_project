30df0e033afcd6a9fe2a81b25bc49ce4bfb74465
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2019-11-06 10:51:24 -0600

    cleaning main

--- Data_structures_final_project/main.cpp
+++ Data_structures_final_project/main.cpp
@@ -3,23 +3,6 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
 	
-	cout<<"Menu"<<endl
-		<<""<<endl;
-	cout<<"otro cambio"<<endl;
-	
-	cout<<"cambio de justin de prueba"<<endl;
-
-	cout<<"cambiore de luis de prueba"<<endl;
-
-	cout<<"otro cambio de luis"<<endl;
-	int hola;
-	cout<<"test final"<<endl;
-	cin>>hola;//cambio de mierda con esa aplicacion de mierda que la mierda de proyecto 
-	//que pidio la mierda de profesor que tenemos en esta mierda de universidad que no me da la plata para el audifono
-	Cout<<"test final punto de vista de JUZZZZZZ"<<endl;
-	
-	//ME CAGO EN GIT
-	
 	
 	return 0;
 }
