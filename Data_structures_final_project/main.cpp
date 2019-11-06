6e6038b97be20eb1900023013ad45795f998b9c8
Justin Blanco <JuzzWhitePage@gmail.com>
2019-11-05 19:37:17 -0600

    el mensaje

--- Data_structures_final_project/main.cpp
+++ Data_structures_final_project/main.cpp
@@ -14,7 +14,8 @@ int main (int argc, char *argv[]) {
 	cout<<"otro cambio de luis"<<endl;
 	int hola;
 	cout<<"test final"<<endl;
-
+	cin>>hola;//cambio de mierda con esa aplicacion de mierda que la mierda de proyecto 
+	//que pidio la mierda de profesor que tenemos en esta mierda de universidad que no me da la plata para el audifono
 	Cout<<"test final punto de vista de JUZZZZZZ"<<endl;
 	
 	//ME CAGO EN GIT
